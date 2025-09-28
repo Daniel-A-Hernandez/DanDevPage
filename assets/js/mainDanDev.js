16079b9687a3209a231efb8e9e1cce725cf418ed
Daniel Hernandez <danny20031405@gmail.com>
2025-09-28 02:21:50 -0600

    UPDATE! Added loader and no scrolling function on loading screen.

--- assets/js/mainDanDev.js
+++ assets/js/mainDanDev.js
@@ -1,3 +1,15 @@
 //Manejo de eventos scroll
 //scroll events
 
+
+//function for not scroll on loader
+window.onload = function () {
+
+    const loader = document.querySelector('.background_loader')
+
+    setTimeout(()=>{
+        loader.style.display = 'none'
+        document.body.style.overflow = 'auto'
+    }, 3500)
+}
+//End
