76b61a9f611d76ca05437a42b1c93c64b4322897
Daniel Hernandez <danny20031405@gmail.com>
2025-09-28 12:48:31 -0600

    UPDATE! Added header, created header change animation in js and styled in css.

--- assets/js/mainDanDev.js
+++ assets/js/mainDanDev.js
@@ -13,3 +13,17 @@ window.onload = function () {
     }, 3500)
 }
 //End
+
+//Header Animation
+
+
+const header = document.querySelector('.head')
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 20){
+        header.classList.add('scrolled') 
+    }
+    else{
+        header.classList.remove('scrolled')
+    }
+
+})
